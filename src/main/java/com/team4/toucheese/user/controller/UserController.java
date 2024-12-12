d640505c5c47fcfde2a2e36d334d1c3dcab039ac
kokiuk <kgukid38@naver.com>
2024-12-12 17:22:48 +0900

    [Feat]북마크 return 타입 void -> ResponseEntity로 수정

--- src/main/java/com/team4/toucheese/user/controller/UserController.java
+++ src/main/java/com/team4/toucheese/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.team4.toucheese.user.controller;
 
 import com.team4.toucheese.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,11 +13,21 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/bookmark/{userId}/{studioId}")
-    public void addBookMark(@PathVariable("userId") Long userId, @PathVariable("studioId") Long studioId){
-        userService.addBookMark(userId, studioId);
+    public ResponseEntity<String> addBookMark(@PathVariable("userId") Long userId, @PathVariable("studioId") Long studioId){
+        try{
+            userService.addBookMark(userId, studioId);
+            return ResponseEntity.ok("success");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
     @DeleteMapping("/bookmark/{userId}/{studioId}")
-    public void deleteBookMark(@PathVariable("userId") Long userId, @PathVariable("studioId") Long studioId){
-        userService.deleteBookMark(userId, studioId);
+    public ResponseEntity<String> deleteBookMark(@PathVariable("userId") Long userId, @PathVariable("studioId") Long studioId){
+        try{
+            userService.deleteBookMark(userId, studioId);
+            return ResponseEntity.ok("success");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }

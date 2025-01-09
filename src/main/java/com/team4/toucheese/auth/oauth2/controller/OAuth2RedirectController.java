3de98f989ff81ccdcba4b3342ea0387ccc01276d
kokiuk <kgukid38@naver.com>
2025-01-09 17:41:02 +0900

    [Feat]에러를 잡기위한 수정

--- src/main/java/com/team4/toucheese/auth/oauth2/controller/OAuth2RedirectController.java
+++ src/main/java/com/team4/toucheese/auth/oauth2/controller/OAuth2RedirectController.java
@@ -1,6 +1,7 @@
 package com.team4.toucheese.auth.oauth2.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,14 +12,11 @@ import java.io.IOException;
 @RestController
 public class OAuth2RedirectController {
     @PostMapping("/user/auth/kakao/callback")
-    public void redirectKakao(
+    public ResponseEntity<String> redirectKakao(
             HttpServletResponse response,
-            @RequestParam("code") String code,
-            @RequestParam("state") String state
-    )throws IOException {
-        String redirectUrl = String.format(
-                "https://www.toucheeseapi.shop/login/oauth2/code/kakao?code=%s&state=%s", code, state
-        );
-        response.sendRedirect(redirectUrl);
+            @RequestParam("code") String code
+    ){
+        System.out.println("response = " + response.toString());
+        return ResponseEntity.ok(code);
     }
 }
